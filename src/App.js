63d2921ae16a8005e2b59fe16531bbe8d9edf880
christphrd <diep.christopher@gmail.com>
2018-09-28 23:15:35 -0400

    imessage style header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter} from 'react-router-dom';
+
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Christopher Diep</h1>
+            <h4>software engineer</h4>
+            <NavBar />
+          </header>
+        </div>
+      </BrowserRouter>
     );
   }
 }
