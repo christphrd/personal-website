01caf6d949c31cca28797f4c64b0fb5487fe05b3
christphrd <diep.christopher@gmail.com>
2018-10-09 12:34:30 -0400

    about route

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const About = () => {
-    return (
-        <div className="About">
-            About
-        </div>
-    )
+class About extends React.Component {
+    componentDidMount() {
+        this.props.history.push('/about')
+    }
+
+    render() {
+        return (
+            <div className="About">
+                About
+            </div>
+        )
+    }
 }
 
 export default About
