3e6ca9b3d2a0b2166fcb7c156756761a7339c9c3
christphrd <diep.christopher@gmail.com>
2018-10-15 17:57:23 -0400

    about page img and desc rough draft

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import blueSuit from '../img/blue-suit.jpg'
+
 class About extends React.Component {
     componentDidMount() {
         this.props.history.push('/about')
@@ -8,7 +10,8 @@ class About extends React.Component {
     render() {
         return (
             <div className="About">
-                About
+                {/* <img src={blueSuit} alt="blue-suit"></img> */}
+                <p>Full stack web developer with a strong quantitative background.With experience in Ruby on Rails, JavaScript, and React, I can build apps that manipulate data.I analyzed experimental data in chemistry.My background in tech support involved working with database integration software. I discovered my passion for coding as I grew in my last role.</p>
             </div>
         )
     }
